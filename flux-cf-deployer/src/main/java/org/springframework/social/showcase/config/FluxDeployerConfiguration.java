7394ee1b7e194ecef6deb659c77fcc3e0057e0aa
Kris De Volder <kdevolder@gopivotal.com>
2014-09-24 12:43:59 -0700

    No more local signup/signin.
    Thanks to Craig Walls.

--- flux-cf-deployer/src/main/java/org/springframework/social/showcase/config/FluxDeployerConfiguration.java
+++ flux-cf-deployer/src/main/java/org/springframework/social/showcase/config/FluxDeployerConfiguration.java
@@ -16,133 +16,80 @@
 
 package org.springframework.social.showcase.config;
 
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.autoconfigure.social.SocialWebAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.social.UserIdSource;
 import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
 import org.springframework.social.connect.Connection;
-import org.springframework.social.connect.ConnectionFactory;
+import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
+import org.springframework.social.connect.web.ProviderSignInController;
 import org.springframework.social.showcase.flux.support.Flux;
 import org.springframework.social.showcase.flux.support.FluxConnectionFactory;
+import org.springframework.social.showcase.signin.SimpleSignInAdapter;
 
-/**
- * {@link EnableAutoConfiguration Auto-configuration} for Spring Social connectivity with
- * Twitter.
- * 
- * @author Craig Walls
- * @author Kris De Volder
- * @since ???
- */
 @Configuration
-@AutoConfigureBefore(SocialWebAutoConfiguration.class)
-@AutoConfigureAfter(WebMvcAutoConfiguration.class)
-public class FluxDeployerConfiguration {
+@EnableSocial
+public class FluxDeployerConfiguration extends SocialConfigurerAdapter {
 
-//	@Configuration
-//	@EnableSocial
-//	@ConditionalOnWebApplication
-//	protected static class GitAutoConfigurationAdapter extends SocialConfigurerAdapter implements
-//	EnvironmentAware {
-//
-//		private RelaxedPropertyResolver properties;
-//
-//		protected String getPropertyPrefix() {
-//			return "spring.social.github.";
-//		}
-//
-//		protected ConnectionFactory<?> createConnectionFactory(RelaxedPropertyResolver properties) {
-//			return new GitHubConnectionFactory(properties.getRequiredProperty("appId"),
-//					properties.getRequiredProperty("appSecret"));
-//		}
-//
-//		@Override
-//		public void addConnectionFactories(ConnectionFactoryConfigurer configurer,
-//				Environment environment) {
-//			//Copied from spring social code. Dont really understand why it can't just use 'environment'
-//			// parameter here instead of implementing 'EnvironmentAware'
-//			configurer.addConnectionFactory(createConnectionFactory(this.properties));
-//		}
-//		
-//		@Bean
-//		@Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
-//		public GitHub github(ConnectionRepository repository) {
-//			Connection<GitHub> connection = repository.findPrimaryConnection(GitHub.class);
-//			if (connection != null) {
-//				return connection.getApi();
-//			}
-//// The twitter one has something like this: do we need that for github? It seems to work fine without this
-////			String id = getProperties().getRequiredProperty("app-id");
-////			String secret = getProperties().getRequiredProperty("app-secret");
-////			return new GitHubTemplate(id, secret);
-//			throw new RuntimeException("No github connection.");
-//		}
-//
-//		@Override
-//		public void setEnvironment(Environment environment) {
-//			this.properties = new RelaxedPropertyResolver(environment, getPropertyPrefix());
-//		}
-//
-//// The example doesn't seem to need this so commented out. 		
-////		@Bean(name = { "connect/githubConnect", "connect/githubConnected" })
-////		@ConditionalOnProperty(prefix = "spring.social.", value = "auto-connection-views")
-////		public View githubConnectView() {
-////			return new GenericConnectionStatusView("github", "GitHub");
-////		}
-//
-//	}
-
-	@Configuration
-	@EnableSocial
-	@ConditionalOnWebApplication
-	protected static class FluxConfigrationAdapter extends SocialConfigurerAdapter {
-
-		protected ConnectionFactory<?> createConnectionFactory(Environment environment) {
-			return new FluxConnectionFactory(
-					environment.getRequiredProperty("cfd.flux.host"),
-					environment.getRequiredProperty("cfd.flux.github.client.id"),
-					environment.getRequiredProperty("cfd.flux.github.client.secret")
-			);
-		}
-
-		@Override
-		public void addConnectionFactories(ConnectionFactoryConfigurer configurer,
-				Environment environment) {
-			configurer.addConnectionFactory(createConnectionFactory(environment));
-		}
-		
-		@Bean
-		@Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
-		public Flux flux(ConnectionRepository repository) {
-			Connection<Flux> connection = repository.findPrimaryConnection(Flux.class);
-			if (connection != null) {
-				return connection.getApi();
+	@Inject
+	private DataSource dataSource;
+	
+	@Override
+	public UserIdSource getUserIdSource() {
+		return new UserIdSource() {	
+			@Override
+			public String getUserId() {
+				Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+				if (authentication == null) {
+					throw new IllegalStateException("Unable to get a ConnectionRepository: no user signed in");
+				}
+				return authentication.getName();
 			}
-			throw new RuntimeException("No github connection.");
-		}
+		};
+	}
 
-//		@Override
-//		public void setEnvironment(Environment environment) {
-//			this.properties = new RelaxedPropertyResolver(environment, getPropertyPrefix());
-//		}
-
-// The example doesn't seem to need this so commented out. 		
-//		@Bean(name = { "connect/githubConnect", "connect/githubConnected" })
-//		@ConditionalOnProperty(prefix = "spring.social.", value = "auto-connection-views")
-//		public View githubConnectView() {
-//			return new GenericConnectionStatusView("github", "GitHub");
-//		}
-
+	@Override
+	public void addConnectionFactories(ConnectionFactoryConfigurer connectionFactoryConfigurer, Environment env) {
+		connectionFactoryConfigurer.addConnectionFactory(new FluxConnectionFactory(
+				env.getRequiredProperty("cfd.flux.host"),
+				env.getRequiredProperty("cfd.flux.github.client.id"),
+				env.getRequiredProperty("cfd.flux.github.client.secret")));
 	}
 	
+	/**
+	 * Singleton data access object providing access to connections across all users.
+	 */
+	@Override
+	public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
+		JdbcUsersConnectionRepository repository = new JdbcUsersConnectionRepository(dataSource, connectionFactoryLocator, Encryptors.noOpText());
+		repository.setConnectionSignUp(new ImplicitConnectionSignUp(dataSource));
+		return repository;
+	}
+
+//	@Bean
+//	@Scope(value="request", proxyMode=ScopedProxyMode.INTERFACES)
+//	public Flux flux(ConnectionRepository repository) {
+//		Connection<Flux> connection = repository.findPrimaryConnection(Flux.class);
+//		return connection != null ? connection.getApi() : null;
+//	}
+
+	@Bean
+	public ProviderSignInController providerSignInController(ConnectionFactoryLocator connectionFactoryLocator, UsersConnectionRepository usersConnectionRepository) {
+		return new ProviderSignInController(connectionFactoryLocator, usersConnectionRepository, new SimpleSignInAdapter(new HttpSessionRequestCache()));
+	}
 }
