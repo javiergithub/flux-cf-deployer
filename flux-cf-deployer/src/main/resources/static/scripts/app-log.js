098aac45dcd3c230cc884cd976016fdabc5b44ce
Kris De Volder <kdevolder@gopivotal.com>
2014-09-26 13:06:48 -0700

    app-log should only show relevant messages for current app.

--- flux-cf-deployer/src/main/resources/static/scripts/app-log.js
+++ flux-cf-deployer/src/main/resources/static/scripts/app-log.js
@@ -6,7 +6,8 @@ require(['socket', 'jquery'], function (socket) {
 	console.log('fluxUser = ', fluxUser);
 	console.log('fluxHost = ', fluxHost);
 
-	var username = fluxUser;
+	var username = fluxUser; //injected by server
+	var app = cfApp; //injected by server
 	
 	socket.on('connect', function() {
 		if (username) {
@@ -14,16 +15,6 @@ require(['socket', 'jquery'], function (socket) {
 				'channel' : username
 			}, function(answer) {
 				console.log('connectToChannel', answer);
-				if (answer.connectedToChannel) {
-					return socket.emit('getProjectsRequest', {
-						username: username,
-						callback_id: 0
-					});
-				} else {
-					if (answer.error) {
-						alert("Flux connection couldn't be established. \n"+answer.error);
-					}
-				}
 			});
 		}
 	});
@@ -42,9 +33,12 @@ require(['socket', 'jquery'], function (socket) {
 	}
 	
 	socket.on('cfAppLog', function (msg) {
-//		console.log('cfAppLog', msg);
-		logAppend(msg.stream, msg.msg);
-//		logAppend("STDOUT", "Changed ["+msg.timestamp+"] "+msg.project+"/"+msg.resource);
+		if (app === msg.app) { 
+			logAppend(msg.stream, msg.msg);
+			console.log('app log :', msg);
+		} else {
+			console.log('filtered:', msg);
+		}
 	});
 	socket.on('error', function (err) {
 		logAppend("SOCKETIO", "Error connecting to websocket at: "+fluxHost);
